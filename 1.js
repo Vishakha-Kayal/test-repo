37eebcfbf972f214b99c96093d48aa8dd9399ef7
Vishakha-Kayal <138288780+Vishakha-Kayal@users.noreply.github.com>
2023-10-20 12:05:36 +0530

    initial commit

---
+++ 1.js
@@ -0,0 +1,15 @@
+// var a = [2,4];
+
+// var b = a;
+// b.pop();
+// console.log(b);
+
+var a;
+
+function usea(a){
+    const b = 12;
+    var c = a+b;
+    console.log(c);
+}
+
+usea(2)
---
+++ responsive.js
@@ -0,0 +1,18 @@
+var a = [1,1,2,'3',4]
+
+// a.forEach(function(val){
+//     console.log(val + " hello");
+// })
+// var sum = 0;
+// a.forEach(function(value){
+//     sum = sum+value;
+// })
+// console.log(sum);
+
+// var sum = 0;
+// a.forEach(function(value){
+//     if(typeof value === "number"){
+//         sum = sum+value;
+//     }
+// })
+// console.log(sum);
